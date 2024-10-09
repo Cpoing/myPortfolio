6cc904dbb0dfd8f5979b8eff1113f04b85434113
Ted Lee <tedlee000@hotmail.com>
2024-10-09 00:34:09 -0400

    aligned aboutme elements

--- src/comps/AboutMe.jsx
+++ src/comps/AboutMe.jsx
@@ -1,15 +1,25 @@
+import pfp from "../icons/pfp-modified.png";
+
 function AboutMe() {
   return (
     <div id="about-me" className="AboutMe">
       <h1>About Me</h1>
-      <img alt="pfp" />
+      <img alt="pfp" src={pfp} className="AboutMe-pfp" />
       <div className="AboutMe-horizontal">
         <div className="AboutMe-short">
           <ul>
-            <li>Name: Ted Lee</li>
-            <li>Interests: </li>
-            <li>Degree: B.Eng Software Engineering (Security Stream)</li>
-            <li>Email: tedlee868@gmail.com</li>
+            <li>
+              Name:<br></br> Ted Lee
+            </li>
+            <li>
+              Interests:<br></br>{" "}
+            </li>
+            <li>
+              Email:<br></br> tedlee868@gmail.com
+            </li>
+            <li>
+              Degree:<br></br> B.Eng Software Engineering - Security Stream
+            </li>
           </ul>
         </div>
         <div className="AboutMe-divider"></div>
