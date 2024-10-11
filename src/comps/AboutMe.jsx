fe8531c7e8556ea4bda835df937bfc306b2653dd
Ted Lee <tedlee000@hotmail.com>
2024-10-11 18:31:22 -0400

    Finished About Me section

--- src/comps/AboutMe.jsx
+++ src/comps/AboutMe.jsx
@@ -4,27 +4,54 @@ function AboutMe() {
   return (
     <div id="about-me" className="AboutMe">
       <h1>About Me</h1>
-      <img alt="pfp" src={pfp} className="AboutMe-pfp" />
+      <img alt="Profile Picture" src={pfp} className="AboutMe-pfp" />
       <div className="AboutMe-horizontal">
         <div className="AboutMe-short">
           <ul>
             <li>
-              Name:<br></br> Ted Lee
+              <strong>Name:</strong>
+              <br /> Ted Lee
             </li>
             <li>
-              Interests:<br></br>{" "}
+              <strong>Interests:</strong>
+              <br />
+              <ul>
+                <li>- Software Engineering</li>
+                <li>- Full-stack development</li>
+                <li>- cybersecurity</li>
+                <li>- Systems programming</li>
+              </ul>
             </li>
             <li>
-              Email:<br></br> tedlee868@gmail.com
+              <strong>Email:</strong>
+              <br /> tedlee868@gmail.com
             </li>
             <li>
-              Degree:<br></br> B.Eng Software Engineering - Security Stream
+              <strong>Degree:</strong>
+              <br /> B.Eng Software Engineering - Security Stream
             </li>
           </ul>
         </div>
         <div className="AboutMe-divider"></div>
         <div className="AboutMe-text">
-          <p>lorem</p>
+          <p>
+            I’m a third-year Software Engineering student at York University,
+            specializing in the Security Stream, and on track to graduate in
+            April 2026. My focus is on full-stack web development and
+            cybersecurity, and I’m currently learning systems-level programming
+            with C and C++. I’ve worked on various personal and academic
+            projects using technologies like JavaScript, TypeScript, Python,
+            Java, and the MERN stack.
+          </p>
+          <p>
+            One of my notable projects is <i>RateMyHall.com</i> A web
+            application designed to allow students to review and rate university
+            lecture halls and give voice to their concerns. The app includes
+            features such as image uploads via AWS S3, a user review system, and
+            session management using JWTs. I’m always seeking new challenges to
+            push my development skills and am excited about using my knowledge
+            to build impactful, secure applications.
+          </p>
         </div>
       </div>
     </div>
